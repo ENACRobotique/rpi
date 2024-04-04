e7452eb6a273e50256c83d8f0a5c126c4c6a2eb8
BOTXavier <xavier.dartevel@gmail.com>
2024-04-04 17:42:31 +0200

    graph + dijkstra

---
+++ sw/dijkstra.py
@@ -0,0 +1,100 @@
+import heapq
+
+class PrioQueue(object):
+    def __init__(self, list : list):
+        """
+        Créer une file à priorité à partir d'une liste list :
+        la liste list doit contenir les couples (priorité, element)
+        """
+        self.queue = [[prio, i, elt] for (i, (prio, elt)) in enumerate(list)]
+        heapq.heapify(self.queue)
+
+
+    def __repr__(self):
+        """
+        Représentation chaine de la file a priorite
+        """
+        return '<PrioQueue: {0.queue}>'.format(self)
+
+
+    def decrease_prio(self, elt, new_prio):
+        """
+        Met à jour la priorite de 'elt' dans la file
+        en la remplaçant par 'new_prio'
+        """
+        for i, (_, _, e) in enumerate(self.queue):
+            if e == elt:
+                self.queue[i][0] = new_prio
+                heapq._siftdown(self.queue, 0, i)
+        return self.queue
+        raise KeyError("decrease_prio")
+
+
+    def pop(self):
+        """
+        Extrait de la file à priorité l'élement de priorité minimale
+        Renvoie le couple (priorité, élement) correspondant
+        """
+        prio, _, elt = heapq.heappop(self.queue)
+        return (prio, elt)
+
+
+    def is_empty(self):
+        """
+        Teste si la file à priorité est vide
+        """
+        return len(self.queue) == 0
+
+
+INF = 10000
+
+def initialisation(G,start):
+    L = G.nodes()
+    d = {}
+    for u in L:
+        d[u]= INF
+    d[start]=0
+    
+    return d
+
+def find_min(Q : PrioQueue):
+    a=Q
+    b = a.pop()
+    return b[1]
+
+def maj_dist(s1, s2, predecesseur : dict, d :dict , G):
+    a = G.weights[(s1,s2)]
+    if d[s2] > d[s1] + a :
+        d[s2] = d[s1] + a
+        predecesseur[s2] = s1
+
+    return d,predecesseur
+
+
+def dijkstra_classic(G,start,end):  #G un graphe
+    """
+    return le chemin le plus court (liste de noeuds successif) et la longueur de ce chemin
+    """
+    d = initialisation(G,start)
+    L = G.nodes()
+    Lq = [(d[u],u) for u in L]
+    Q = PrioQueue(Lq)
+    
+    predecesseur ={}
+    visite = []
+    while not Q.is_empty():
+        u = find_min(Q)
+        visite.append(u)
+        if u == end :
+            break
+        for v in G.neighbours(u):
+            maj_dist(u,v,predecesseur,d,G)
+            Q.decrease_prio(v,d[v])
+    chemin = []
+    s = end
+    while s!=start : 
+        chemin +=[s]
+        s = predecesseur[s]
+    chemin += [start]
+
+    return chemin.reverse(),d[end]
---
+++ sw/map.py
@@ -0,0 +1,107 @@
+import matplotlib.pyplot as plt
+from math import sqrt
+import dijkstra
+import random as rd 
+
+class Graph(object):  
+    def __init__(self):
+        """Cree un graphe vide"""
+        self.adj = {}
+        self.coords = {}
+        self.weights = {}
+
+    def __repr__(self):
+        """Representation en chaine de caracteres d'un graphe"""
+        return '<Graph: {0.adj}>'.format(self)
+
+    def add_node(self, u,x,y):
+        """Ajoute un noeud u au graphe"""
+        self.adj[u] = []
+        self.coords[u] = [x,y]
+
+    def add_edge(self, u, v):
+        """Ajoute l'arete (u, v) au graphe"""
+        self.adj.setdefault(u, []).append(v)
+        self.adj.setdefault(v, []).append(u)
+
+    def size(self):
+        """Renvoie le nombre de noeuds du graphe"""
+        return len(self.adj)
+
+    def nodes(self):
+        """Renvoie la liste des noeuds du graphe"""
+        return self.adj.keys()
+
+    def neighbours(self, u):
+        """Renvoie la liste des voisins du noeud u dans le graphe"""
+        return self.adj[u]
+
+    def weight(self):
+        for point in self.adj :
+            for voisin in self.adj[point]:
+                x1 = self.coords[point][0]
+                x2 = self.coords[voisin][0]
+                y1 = self.coords[point][1]
+                y2 = self.coords[voisin][1]
+                w =  sqrt((x1 - x2)**2 + (y1 - y2)**2)
+                self.weights.setdefault((point,voisin), []).append(w)
+                self.weights.setdefault((voisin,point), []).append(w)
+        
+
+
+def read_graph(file):
+    g = Graph()
+    neighbours = []
+    points = []
+    with open(file) as f:
+        for line in f:
+            l = line.split()
+            g.add_node(l[0],float(l[1]),float(l[2])) #nom x y 
+            points.append(l[0])
+            nom_spliter = l[3].split(',')
+            neighbours.append(nom_spliter)
+    for count, x in enumerate(neighbours):
+        for y in x:
+            g.add_edge(points[count],y)
+    return g
+
+
+def print_map(graph):
+    g = graph 
+    for point in g.adj :
+        plt.annotate(point, [g.coords[point][0] + rd.randint(-2,2)*0, g.coords[point][1]+ rd.randint(-2,2)/25 ])
+        for voisin in g.adj[point]:
+            x1 = g.coords[point][0]
+            y1 = g.coords[point][1]
+            x2 = g.coords[voisin][0]
+            y2 = g.coords[voisin][1]
+            plt.plot([x1,x2],[y1,y2])
+            
+
+
+def print_chemin(g,chemin):
+    for i in range(len(chemin)-1):
+        x1 = g.coords[chemin[i]][0]
+        y1 = g.coords[chemin[i]][1]
+        x2 = g.coords[chemin[i+1]][0]
+        y2 = g.coords[chemin[i+1]][1]
+        plt.plot([x1,x2],[y1,y2],color='r',linewidth=5)
+        
+
+
+file = 'graph.txt'
+graph = read_graph(file) #map de la table
+
+chemin,distance_totale = dijkstra.dijkstra_classic(graph,"plate_B_1", "interet_SE") #a liste des points parcourus,nd distance parcourue
+
+plt.figure()
+print_map(graph)
+
+print_chemin(graph,chemin)
+
+print(chemin)
+print(distance_totale)
+
+print(graph.coords[chemin[1]][0]) #coordonnes x du point 
+#pour obtenir les coords d'un point le la liste a : pt = g.coords["nom_du_point"]
+plt.show()
