dd8802782cc61b8ca7e9a00d91500a13549998b9
Cooking mama <robotique.enac@gmail.com>
2024-04-21 13:06:10 +0200

    aruco service

--- services/install_services.sh
+++ services/install_services.sh
@@ -14,7 +14,7 @@ sudo ln -s $sp/robot_start.service 		/etc/systemd/system/robot_start.service
 #sudo ln -s $sp/robot_strat.service 		/etc/systemd/system/robot_strat.service
 sudo ln -s $sp/robot_vl53.service 		/etc/systemd/system/robot_vl53.service
 sudo ln -s $sp/robot_IO.service 		/etc/systemd/system/robot_IO.service
-
+sudo ln -s $sp/robot_aruco.service 		/etc/systemd/system/robot_aruco.service
 
 #sudo systemctl enable robot_bridge.service
 #sudo systemctl enable robot_lidar_driver.service
@@ -26,6 +26,7 @@ sudo systemctl enable robot_start.service
 #sudo systemctl enable robot_strat.service
 #sudo systemctl enable robot_vl53.service
 sudo systemctl enable robot_IO.service
+sudo systemctl enable robot_aruco.service
 
 sudo systemctl daemon-reload
 
