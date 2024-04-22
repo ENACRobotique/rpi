45eba08068cb63b93ecbae4153466f895f2d4d0a
ENACRobotique <robotique.enac@gmail.com>
2024-04-22 20:45:02 +0200

    LCD subscriber

--- src/lcd/ecal_lcd.py
+++ src/lcd/ecal_lcd.py
@@ -2,6 +2,7 @@
 from lcd_driver import *
 import ecal.core.core as ecal_core
 from ecal.core.publisher import ProtoPublisher
+from ecal.core.subscriber import ProtoSubscriber
 
 import sys, os, time
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
@@ -13,6 +14,8 @@ class ECAL_LCD:
         ecal_core.initialize(sys.argv, "eCAL_LCD_driver")
         self.state_pub = ProtoPublisher("LCDState", rpb.LCDState)
         self.event_pub = ProtoPublisher("LCDEvent", rpb.LCDEvent)
+        self.display_sub = ProtoSubscriber("LCDOut", rpb.LCDOut)
+        self.display_sub.set_callback(self.display_lcd)
         self.lcd = LCD(port, self.lcd_event_cb)
     
     def lcd_event_cb(self, btn: Button, val):
@@ -29,12 +32,15 @@ class ECAL_LCD:
         msg.tirette = int(self.lcd.get_state(Button.TIRETTE))
         msg.potar = self.lcd.get_state(Button.POTAR)
         self.state_pub.send(msg)
+    
+    def display_lcd(self, topic_name, msg, timestamp):
+        self.lcd.display(msg.line1, msg.line2, msg.red, msg.green, msg.blue, msg.buzzer)
 
 
 if __name__ == '__main__':
-    ecal_lcd = ECAL_LCD('/dev/ttyUSB1')
+    ecal_lcd = ECAL_LCD('/dev/robot_lcd')
 
-    while True:
+    while ecal_core.ok():
         time.sleep(1)
         ecal_lcd.send_state()
     
