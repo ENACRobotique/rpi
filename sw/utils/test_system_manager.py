a36dc5e9b2df6d74bdb5000ce98459605f6ddfa1
ENAC-Robotique <robotique.enac@gmail.com>
2024-11-29 00:51:37 +0100

    reoganisation, on t'aime Hugo

--- sw/utils/test_system_manager.py
+++ sw/utils/test_system_manager.py
@@ -9,10 +9,10 @@ import generated.robot_state_pb2 as robot_pb
 import generated.lidar_data_pb2 as lidar_pb
 import generated.messages_pb2 as base_pb
 
-G_NONE = 0
+G_NONE = 0x0
 G_BASIC = 0x1
 
-A_NONE = 0
+A_NONE = 0x0
 A_DIRECT = 0x1
 A_POS = 0x2
 A_SPEED = 0x4
@@ -26,16 +26,18 @@ class SystemManager:
 
 
     def set_mode(self, asserv, guidance):
+        print(asserv,guidance)
         mode = base_pb.System()
         mode.asserv = asserv
         mode.guidance = guidance
         mode.odometry = base_pb.System.OdometryFlags.ODOMETRY_ENABLED
         self.Mode_pub.send(mode)
 
+ecal_core.initialize(sys.argv,"Test mode")
+sleep(1) # laissons ecal se réveiller 
+t = SystemManager()
+
 if __name__ == "__main__":
-    ecal_core.initialize(sys.argv,"Test mode")
-    sleep(1) # laissons ecal se réveiller 
-    t = SystemManager()
     while (True):
         sleep(0.5)
 
