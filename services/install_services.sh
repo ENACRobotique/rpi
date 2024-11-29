e809bd42c1c6b5a0030818868362ed61dbdf7245
ENAC-Robotique <robotique.enac@gmail.com>
2024-11-29 17:30:56 +0100

    better service install script

--- services/install_services.sh
+++ services/install_services.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
+[ $UID != 0 ] && echo A lancer avec sudo && exit 42
+
 sp="/home/pi/rpi2024/services"
 
-sudo rm /etc/systemd/system/robot_*
-sudo rm /etc/systemd/system/pigpiod.service
+rm /etc/systemd/system/robot_*
+rm /etc/systemd/system/pigpiod.service
 
-sudo ln -s $sp/robot_bridge.service 		/etc/systemd/system/robot_bridge.service
-sudo ln -s $sp/robot_lidar_driver.service 	/etc/systemd/system/robot_lidar_driver.service
-sudo ln -s $sp/robot_lidar_clients.service 	/etc/systemd/system/robot_lidar_clients.service
-sudo ln -s $sp/robot_lcd.service 		/etc/systemd/system/robot_lcd.service
-#sudo ln -s $sp/robot_lidar_fusion.service 	/etc/systemd/system/robot_lidar_fusion.service
-#sudo ln -s $sp/robot_lidar_loca.service 	/etc/systemd/system/robot_lidar_loca.service
-#sudo ln -s $sp/robot_optitrack.service 		/etc/systemd/system/robot_optitrack.service
-sudo ln -s $sp/robot_record.service 		/etc/systemd/system/robot_record.service
-sudo ln -s $sp/robot_start.service 		/etc/systemd/system/robot_start.service
-sudo ln -s $sp/robot_strat.service 		/etc/systemd/system/robot_strat.service
-sudo ln -s $sp/robot_vl53.service 		/etc/systemd/system/robot_vl53.service
-sudo ln -s $sp/robot_IO.service 		/etc/systemd/system/robot_IO.service
-sudo ln -s $sp/robot_aruco.service 		/etc/systemd/system/robot_aruco.service
-sudo ln -s $sp/pigpiod.service                  /etc/systemd/system/pigpiod.service
+for f in \
+robot_bridge \
+robot_lidar_driver \
+robot_lidar_clients \
+robot_lcd \
+robot_record \
+robot_start \
+robot_strat \
+robot_vl53 \
+robot_IO \
+# robot_optitrack \
+robot_aruco ; do 
+    ln -s $sp/$f.service 		/etc/systemd/system/$f.service
+done
 
-#sudo systemctl enable robot_bridge.service
-#sudo systemctl enable robot_lidar_driver.service
-#sudo systemctl enable robot_lidar_fusion.service
-#sudo systemctl enable robot_lidar_loca.service
-#sudo systemctl enable robot_optitrack.service
-#sudo systemctl enable robot_record.service
-sudo systemctl enable robot_start.service
-#sudo systemctl enable robot_strat.service
-#sudo systemctl enable robot_vl53.service
-#sudo systemctl enable robot_IO.service
-#sudo systemctl enable robot_aruco.service
+ln -s $sp/pigpiod.service                  /etc/systemd/system/pigpiod.service
 
-sudo systemctl daemon-reload
+systemctl enable robot_start.service
+
+systemctl daemon-reload
 
