eb479c1df284bf2ecf9af274e00cb7d8e1dd4f69
ENAC-Robotique <robotique.enac@gmail.com>
2025-01-16 19:30:33 +0100

    Filtrage des points -> Détection des balises

--- drivers/lidar/balises.py
+++ drivers/lidar/balises.py
@@ -8,39 +8,59 @@ import time, os, sys
 from numpy import typing as npt
 import generated.robot_state_pb2 as robot_pb
 import generated.lidar_data_pb2 as lidar_pb
-import robot_state_pb2 as hgpb
 
 
 # il faut Créer lidar_pb.Coord_beacons 
 
-
+TOLERANCE_PHI_MOINS_1 = 50
 TOLERANCE_T_MOINS_1 = 50
 ERR_TOLERANCE_BEACON_DETECT = 1
+
+old_pos=[
+         0, #x
+         0, #y
+         0 #phi heading
+         ]#position à l'instant n-1 du robot dans le repère de la table
+
+coords_init=[(0,0),(0,1),(1,0)]
+beacons_polar_angles=[0,2*np.pi/6,np.pi]
+
+
+
+
 
 def dist_2_pts(pt1, pt2):
     return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
 
-def trilateration(triplet_pts,d1,d2,d3):
-        p1=triplet_pts[0]
-        p2=triplet_pts[1]
-        p3=triplet_pts[2]
+def trilateration(beacons_abs_position,distances):
+        (x1,y1),(x2,y2),(x3,y3)=beacons_abs_position
+        d1,d2,d3=distances
+        A=np.array([
+            [2*(x2-x1),2*(y2-y1)],
+            [2*(x3-x1),2*(y3-y1)]
+        ])
+        B=np.array([
+            [d1**2-d2**2-x1**2-y1**2+x2**2+y2**2]
+            [d1**2-d3**2-x1**2-y1**2+x3**2+y3**2]
+       ])
+        position = np.linalg.solve(A,B)
+        return position[0][0], position[1][0]
 
-        
-        return (1,1)  
-coords_init=[(0,0),(0,1),(1,0)]
+def get_heading(beacons_polar_angles,angles) : #angles c les angles mesurées par le lidar
+        phi=sum(beacons_polar_angles[i]-angles[i] for i in range(3))/3    #On prend la moyenne pour qu'il y ait moins d'erreurs
+        phi=np.arctan2(np.sin(phi),np.cos(phi))
+        return phi
+
 class Balises:
     def __init__(self):
         self.coords_c_rr=coords_init #coordonnées cartésiennes des balises réferentiel robot
         self.odom_pos=(0,0)
         ecal_core.initialize(sys.argv, "lid_amalgameur")
         self.sub_lidar = ProtoSubscriber("amalgames", lidar_pb.Amalgames)
-        ecal_core.initialize(sys.argv, "Bridge low level")
-        self.odom_pos_pub = ProtoSubscriber("odom_pos", hgpb.Position)
-        self.odom_pos_pub.set_callback(self.odompos)
         self.sub_lidar.set_callback(self.beacon_detection)
+        self.old_pos_robot=old_pos
+
        # self.pub_coord_beacons = ProtoPublisher("coord beacons", lidar_pb.Coord_beacons)
-    def odompos(self,topic_name,msg,time):
-        self.odom_pos=(msg.x,msg.y)
 
     def beacon_detection(self, topic_name, msg, time):
         x = np.array(msg.x)
@@ -57,15 +77,10 @@ class Balises:
         for triplets in combinations(points, 3):
 
 
-            distances_triplet = sorted([d1, d2, d3])
+            distances_triplet = sorted([d1_b, d2_b, d3_b])
                     
-                
-            if dist_2_pts(trilateration(triplets),self.odom_pos)<= TOLERANCE_T_MOINS_1:
-                d1 = dist_2_pts(triplets[0], triplets[1])  
-                d2 = dist_2_pts(triplets[0], triplets[2])
-                d3 = dist_2_pts(triplets[1], triplets[2])
-
-            
+            last_x,last_y,last_phi=self.old_pos_robot
+            if dist_2_pts(trilateration(coords_init,self.coords_c_rr),(self.odom_pos))<= TOLERANCE_T_MOINS_1 and get_heading(beacons_polar_angles,angles=(0,0,0))-last_phi <TOLERANCE_PHI_MOINS_1:       #il faut récupérer les angles depuis les données lida     
                 if all(abs(distance_balise[i] - distances_triplet[i]) <= ERR_TOLERANCE_BEACON_DETECT for i in range(3)):
                     return self.coords_c_rr
                 
