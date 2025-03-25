379675d506e683812f1fef8c052d431dba1203fc
Hugo <hugo.louis.vitry@gmail.com>
2025-03-25 22:32:48 +0100

    updating map for 2025 edition

--- sw/map.py
+++ sw/map.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from math import sqrt
 #import dijkstra
 import random as rd 
@@ -71,31 +72,71 @@ def read_graph(file):
     return g
 
 
-def print_map(graph):
+def print_map(graph, ax = None):
     """
     Affiche la map de la table avec les chemins du graph
     """
     g = graph 
     for point in g.adj :
-        plt.annotate(point, [g.coords[point][0] + rd.randint(-2,2)*0, g.coords[point][1]+ rd.randint(-2,2)/25 ])
+        plt.annotate(point, [g.coords[point][0] + rd.randint(-2,2)*0, g.coords[point][1]+ rd.randint(-2,2)/25 ],fontsize=14)
         for voisin in g.adj[point]:
             x1 = g.coords[point][0]
             y1 = g.coords[point][1]
             x2 = g.coords[voisin][0]
             y2 = g.coords[voisin][1]
             plt.plot([x1,x2],[y1,y2],color='black')
-    #affichage des jardinières
-    plt.plot([0,0],[450,450+325],color='y')
-    plt.plot([0,0],[2000-450-325,2000-450],color='b')
-    plt.plot([600,925],[2000,2000],color='b')
-
-    plt.plot([3000,3000],[450,450+325],color='b')
-    plt.plot([3000,3000],[2000-450-325,2000-450],color='y')
-    plt.plot([3000-600,3000-925],[2000,2000],color='y')
-
     
-            
+    #Affichage des bords de table
+    table = patches.Rectangle((0,0),3000,2000,linewidth=2, edgecolor='black', facecolor='none')
+    ax.add_patch(table)
+    
+    #Affichage des zones Jaunes
+    j1 = patches.Rectangle((1000,0),450,450,linewidth=1, color='yellow')
+    j2 = patches.Rectangle((2550,650),450,450,linewidth=1, color='yellow')
+    j3 = patches.Rectangle((150,1550),450,450,linewidth=1, color='yellow')
+    ax.add_patch(j1)
+    ax.add_patch(j2)
+    ax.add_patch(j3)
+    
+    #Affichage des zones Bleu
+    b1 = patches.Rectangle((1550,0),450,450,linewidth=1, color='lightblue')
+    b2 = patches.Rectangle((0,650),450,450,linewidth=1, color='lightblue')
+    b3 = patches.Rectangle((2400,1550),450,450,linewidth=1, color='lightblue')
+    ax.add_patch(b1)
+    ax.add_patch(b2)
+    ax.add_patch(b3)
 
+    #Affichage des stocks 
+    s1 = patches.Rectangle((25,200),100,400,linewidth=1, color='burlywood')
+    s2 = patches.Rectangle((2875,200),100,400,linewidth=1, color='burlywood')
+    s3 = patches.Rectangle((25,1125),100,400,linewidth=1, color='burlywood')
+    s4 = patches.Rectangle((2875,1125),100,400,linewidth=1, color='burlywood')
+    s5 = patches.Rectangle((575,200),400,100,linewidth=1, color='burlywood')
+    s6 = patches.Rectangle((2025,200),400,100,linewidth=1, color='burlywood')
+    s7 = patches.Rectangle((1700,900),400,100,linewidth=1, color='burlywood')
+    s8 = patches.Rectangle((900,900),400,100,linewidth=1, color='burlywood')
+    s9 = patches.Rectangle((625,1675),400,100,linewidth=1, color='burlywood')
+    s10 = patches.Rectangle((1975,1675),400,100,linewidth=1, color='burlywood')
+    ax.add_patch(s1)
+    ax.add_patch(s2)
+    ax.add_patch(s3)
+    ax.add_patch(s4)
+    ax.add_patch(s5)
+    ax.add_patch(s6)
+    ax.add_patch(s7)
+    ax.add_patch(s8)
+    ax.add_patch(s9)
+    ax.add_patch(s10)
+    
+    #Affichage des zones de construction par équipe
+    c1 = patches.Rectangle((0,0),450,150,linewidth=1, color='lightblue')
+    c2 = patches.Rectangle((2000,0),450,150,linewidth=1, color='lightblue')
+    c3 = patches.Rectangle((555,0),450,150,linewidth=1, color='yellow')
+    c4 = patches.Rectangle((2550,0),450,150,linewidth=1, color='yellow')
+    ax.add_patch(c1)
+    ax.add_patch(c2)
+    ax.add_patch(c3)
+    ax.add_patch(c4)
 
 def print_chemin(g,chemin):
     """
@@ -118,8 +159,8 @@ if __name__ == "__main__" :
     #chemin,distance_totale = dijkstra.dijkstra_classic(graph,"secureB", "potSE") #a liste des points parcourus,nd distance parcourue
     
     print(graph.weights)
-    plt.figure()
-    print_map(graph)
+    fig, ax = plt.subplots()
+    print_map(graph,ax)
 
     #print_chemin(graph,chemin)
 
@@ -128,6 +169,9 @@ if __name__ == "__main__" :
 
     #print(graph.coords[chemin[1]][0]) #coordonnes x du point 
     #pour obtenir les coords d'un point le la liste a : pt = g.coords["nom_du_point"]
-    plt.grid()
+    # plt.grid(True, color='black', linestyle='--', linewidth=0.5)
+    plt.title("Carte Très Basique",fontsize=16)
+    plt.xticks(size=16)
+    plt.yticks(size=16)
     plt.axis('equal')
     plt.show()
