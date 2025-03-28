2ecf13fd1eb5ae29bdfec637254b61b504bc550a
ENACRobotique <robotique.enac@gmail.com>
2025-03-29 00:26:12 +0100

    manual IO controll

--- drivers/joystick/joystick.py
+++ drivers/joystick/joystick.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append("../..")
 import generated.robot_state_pb2 as robot_state_pb2
 import generated.messages_pb2 as message_pb2
-
+from sw.actionneurs import IO_Manager
 
 
 MAX_SPEED = 600
@@ -16,7 +16,9 @@ V_THETA = 3
 
 FRAME_W = 1
 FRAME_R = 0
-
+UP = 1
+DOWN = -1
+NO = 0
 ATTACK3_CONF = {
     "X": 1, #axe
     "X_sens" : -1, #facteur
@@ -47,17 +49,28 @@ BATTLETRON = {
     "frame_robot":1, #bouton CARRÉ
     "frame_table":3, #bouton ROND
     "theta_supra_luminique":11, #bouton
-    "vitesse_supra_luminique": 12 #bouton
+    "vitesse_supra_luminique": 12, #bouton
+    "glisse" : 1, #hat (fleche haut et bas)
+    "selectGlisse" : 0, #hat (fleche gauche et droite) 
+    "bras" : 6, #bouton gachette gauche
+    "verrou" : 7, #bouton gachette droite
+    "grabHaut" : 4, #bouton bumper gauche
+    "grabBas" : 5 #bouton bumper droit
+
 }
 
-
+PLANCHE = 0
+RENTREUR = 1
+ASSCENSEUR = 2
 class JoystickEcal ():
     def __init__(self):
         self.joystick: Joystick = None
         self.buttons = []
         self.axis = []
-        self.conf = None
+        self.hats = []
+        self.conf = BATTLETRON
         self.frame = FRAME_R
+        self.glisseMode = PLANCHE
 
         ecal_core.initialize([], "Joystick")
         time.sleep(1) # on laisse ecal se reveiller
@@ -65,10 +78,16 @@ class JoystickEcal ():
         self.speed_publisher = ProtoPublisher("speed_cons", robot_state_pb2.Speed)
         self.message = robot_state_pb2.Speed()
         self.Mode_pub = ProtoPublisher("system_modes",message_pb2.System)
+        self.IO_manager = IO_Manager()
+
+        self.verrouLock = False
+        self.brasUP = False
+        self.grabHaut = False
+        self.grabBas = False
         # self.change_frame() # on passe en frame Robot
         
     def __repr__(self):
-        return f"{len(self.axis)} Axis: {self.axis} \t{len(self.buttons)} Buttons : {self.buttons}"
+        return f"{len(self.axis)} Axis: {self.axis} \t{len(self.buttons)} Buttons : {self.buttons} \t{len(self.hats)} Hats : {self.hats}"
 
     def open(self):
         while (self.joystick == None):
@@ -77,6 +96,7 @@ class JoystickEcal ():
                     self.joystick = pygame.joystick.Joystick(event.device_index)
                     self.axis = [self.axis_get_value(n) for n in range(self.joystick.get_numaxes())]
                     self.buttons = [self.button_get_value(n) for n in range(self.joystick.get_numbuttons())]
+                    self.hats = [self.hats_get_value(n) for n in range(self.joystick.get_numhats())]
                     time.sleep(0.5)
                     print(f"Manette {self.joystick.get_name()} connectée")
     def set_conf(self, conf):
@@ -88,6 +108,9 @@ class JoystickEcal ():
     
     def axis_get_value(self,n):
             return round(self.joystick.get_axis(n),2)
+    
+    def hats_get_value(self,n):
+            return self.joystick.get_hat(n)
 
     def update_value(self):
         for n in range(self.joystick.get_numbuttons()):
@@ -95,6 +118,9 @@ class JoystickEcal ():
 
         for n in range(self.joystick.get_numaxes()):
             self.axis[n] = self.axis_get_value(n)
+        
+        for n in range(self.joystick.get_numhats()):
+            self.hats[n] = self.hats_get_value(n)
 
     def publish_command(self):
         vx = (MAX_SPEED * self.axis[self.conf["X"]] * self.conf["X_sens"] - self.conf["X_offset"]) * (1 + self.buttons[self.conf["vitesse_supra_luminique"]])
@@ -103,10 +129,44 @@ class JoystickEcal ():
         self.message.vy = vy if abs(self.axis[self.conf["Y"]]) > self.conf["Y_dead_zone"] else 0
         
         if self.conf == BATTLETRON:
-
-
             vtheta = (V_THETA * self.axis[self.conf["THETA"]] * self.conf["THETA_sens"] - self.conf["THETA_offset"]) * (1 + self.buttons[self.conf["theta_supra_luminique"]])
             self.message.vtheta = vtheta if abs(self.axis[self.conf["THETA"]]) > self.conf["THETA_dead_zone"] else 0
+            
+            #Actionneur 2025
+            if self.hats[0][self.conf["selectGlisse"]] != 0:
+                self.glisseMode +=self.hats[0][self.conf["selectGlisse"]]
+                
+            if self.glisseMode == PLANCHE:
+                self.IO_manager.liftPlancheContinu(self.hats[0][self.conf["glisse"]])
+                
+            if self.glisseMode == RENTREUR:
+                self.IO_manager.stockConserveContinu(self.hats[0][self.conf["glisse"]])
+                
+            if self.glisseMode == ASSCENSEUR:
+                self.IO_manager.liftConserveContinu(self.hats[0][self.conf["glisse"]])
+            
+            if self.buttons[self.conf["verrou"]] == 1:
+                self.verrouLock = not self.verrouLock
+                self.IO_manager.lockPlanche(self.verrouLock)
+                time.sleep(0.25)
+
+            if self.buttons[self.conf["bras"]] == 1:
+                self.brasUP = not self.brasUP
+                self.IO_manager.deployPince(self.brasUP)
+                time.sleep(0.25)
+
+            if self.buttons[self.conf["grabHaut"]] == 1:
+                self.grabHaut = not self.grabHaut
+                self.IO_manager.grabHighConserve(self.grabHaut)
+                time.sleep(0.25)
+
+            if self.buttons[self.conf["grabBas"]] == 1:
+                self.grabBas = not self.grabBas
+                self.IO_manager.grabLowConserve(self.grabBas)
+                time.sleep(0.25)
+            
+            
+
         if self.conf == ATTACK3_CONF:
             self.message.vtheta = V_THETA * (self.buttons[self.conf["angle_gauche"]] - self.buttons[self.conf["angle_droit"]]) * (1 + self.buttons[self.conf["vitesse_supra_luminique"]])
         
@@ -150,8 +210,10 @@ if __name__ == '__main__':
 
     while True :
         for event in pygame.event.get():
+            # print(event)
             joysticks_ecal.update_value()
-            #print(joysticks_ecal)
+            print(f'Glissière :{joysticks_ecal.glisseMode}\n')
+            # print(joysticks_ecal)
         joysticks_ecal.publish_command()  
         time.sleep(0.1)
     
--- sw/actionneurs.py
+++ sw/actionneurs.py
@@ -1,7 +1,8 @@
 from enum import Enum
-from ..drivers.smart_servo.test.ecalServoIO import servoIO
+import sys
+sys.path.append("../")
+from drivers.smart_servo.test.ecalServoIO import servoIO
 import generated.actionneurs_pb2 as actionneurs_pb
-
 class Actionneur(Enum):
     #todo : Droit = la droite du robot par rapport à son axe X
     AimantBasDroit = 4
@@ -20,25 +21,25 @@ class Actionneur(Enum):
     AscenseurBanderolle = 10
 
 class ValeurActionneur(Enum):
-    STSLowSpeed = 0
-    AimantBasDroitGRAB = 0
-    AimantBasDroitDROP = 0
-    AimantBasGaucheGRAB = 0
-    AimantBasGaucheDROP = 0
-    AimantHautDroitGRAB = 0
-    AimantHautDroitDROP = 0
-    AimantHautGaucheGRAB = 0
-    AimantHautGaucheDROP = 0
+    STSLowSpeed = 3000
+    AimantBasDroitGRAB = 3100
+    AimantBasDroitDROP = 3800
+    AimantBasGaucheGRAB = 2950
+    AimantBasGaucheDROP = 2285
+    AimantHautDroitGRAB = 1900
+    AimantHautDroitDROP = 3000
+    AimantHautGaucheGRAB = 1700
+    AimantHautGaucheDROP = 1100
     AscenseurAimantUP = 0
     AscenseurAimantDOWN = 0
 
     RentreurIN = 0
     RentreurOUT = 0
 
-    BrasPinceIN = 0
-    BrasPinceOUT = 0
-    VerrouPinceLOCK = 0
-    VerrouPinceUNLOCK = 0
+    BrasPinceIN = 500
+    BrasPinceOUT = 200
+    VerrouPinceLOCK = 2300
+    VerrouPinceUNLOCK = 3500
 
     PlancheDroitUP = 0
     PlancheDroitDOWN = 0
@@ -55,67 +56,105 @@ class IO_Manager:
     def initActionneur(self):
         """Passage de tout les actionneurs à leur position de début de match \n"""
         pass
-        
+    
+    def liftPlancheContinu(self, direction, sync:bool =False):
+        self.Servo_IO.setEndless(Actionneur.PlancheDroit.value,True)
+        self.Servo_IO.setEndless(Actionneur.PlancheGauche.value,True)
+        if direction == 1 :
+            self.Servo_IO.turn(Actionneur.PlancheDroit.value,1,ValeurActionneur.STSLowSpeed.value)
+            self.Servo_IO.turn(Actionneur.PlancheGauche.value,0,ValeurActionneur.STSLowSpeed.value)
+            
+        elif direction == -1:
+            self.Servo_IO.turn(Actionneur.PlancheDroit.value,0,ValeurActionneur.STSLowSpeed.value)
+            self.Servo_IO.turn(Actionneur.PlancheGauche.value,1,ValeurActionneur.STSLowSpeed.value)
+
+        else :
+            self.Servo_IO.turn(Actionneur.PlancheDroit.value,0,0)
+            self.Servo_IO.turn(Actionneur.PlancheGauche.value,0,0)
+    
+    def stockConserveContinu(self, direction, sync:bool =False):
+        self.Servo_IO.setEndless(Actionneur.Rentreur.value,True)
+        if direction == 1 :
+            self.Servo_IO.turn(Actionneur.Rentreur.value,0,ValeurActionneur.STSLowSpeed.value)
+
+        elif direction == -1:
+            self.Servo_IO.turn(Actionneur.Rentreur.value,1,ValeurActionneur.STSLowSpeed.value)
+
+        else :
+            self.Servo_IO.turn(Actionneur.Rentreur.value,0,0)
+
+    def liftConserveContinu(self, direction, sync:bool =False):
+        self.Servo_IO.setEndless(Actionneur.AscenseurAimant.value,True)
+        if direction == 1 :
+            self.Servo_IO.turn(Actionneur.AscenseurAimant.value,0,ValeurActionneur.STSLowSpeed.value)
+
+        elif direction == -1:
+            self.Servo_IO.turn(Actionneur.AscenseurAimant.value,1,ValeurActionneur.STSLowSpeed.value)
+
+        else :
+            self.Servo_IO.turn(Actionneur.AscenseurAimant.value,0,0)
+
+
     def liftPlanches(self, up:bool, sync:bool = False):
         """Monter ou descendre les planches\n"""
         if up:
-            self.Servo_IO.moveSpeed(Actionneur.PlancheDroit, ValeurActionneur.PlancheDroitUP, ValeurActionneur.STSLowSpeed)
-            self.Servo_IO.moveSpeed(Actionneur.PlancheGauche, ValeurActionneur.PlancheGaucheUP, ValeurActionneur.STSLowSpeed)
+            self.Servo_IO.moveSpeed(Actionneur.PlancheDroit.value, ValeurActionneur.PlancheDroitUP.value, ValeurActionneur.STSLowSpeed.value)
+            self.Servo_IO.moveSpeed(Actionneur.PlancheGauche.value, ValeurActionneur.PlancheGaucheUP.value, ValeurActionneur.STSLowSpeed.value)
             
         else:
-            self.Servo_IO.moveSpeed(Actionneur.PlancheDroit, ValeurActionneur.PlancheDroitDOWN, ValeurActionneur.STSLowSpeed)
-            self.Servo_IO.moveSpeed(Actionneur.PlancheGauche, ValeurActionneur.PlancheGaucheDOWN, ValeurActionneur.STSLowSpeed)
+            self.Servo_IO.moveSpeed(Actionneur.PlancheDroit.value, ValeurActionneur.PlancheDroitDOWN.value, ValeurActionneur.STSLowSpeed.value)
+            self.Servo_IO.moveSpeed(Actionneur.PlancheGauche.value, ValeurActionneur.PlancheGaucheDOWN.value, ValeurActionneur.STSLowSpeed.value)
             
         
     def lockPlanche(self, lock:bool, sync:bool = False):
         """ Tenir ou lacher la planche du haut"""
         if lock:
-            self.Servo_IO.move(Actionneur.VerrouPince, ValeurActionneur.VerrouPinceLOCK)
+            self.Servo_IO.move(Actionneur.VerrouPince.value, ValeurActionneur.VerrouPinceLOCK.value)
             
         else :
-            self.Servo_IO.move(Actionneur.VerrouPince, ValeurActionneur.VerrouPinceUNLOCK)
+            self.Servo_IO.move(Actionneur.VerrouPince.value, ValeurActionneur.VerrouPinceUNLOCK.value)
             
         
     def deployPince(self, deploy:bool, sync:bool = False):
         if deploy:
-            self.Servo_IO.move(Actionneur.BrasPince, ValeurActionneur.BrasPinceOUT, actionneurs_pb.SmartServo.ServoType.AX12)
+            self.Servo_IO.moveSpeed(Actionneur.BrasPince.value, ValeurActionneur.BrasPinceOUT.value,100, actionneurs_pb.SmartServo.ServoType.AX12)
             
         else :
-            self.Servo_IO.move(Actionneur.BrasPince, ValeurActionneur.BrasPinceIN, actionneurs_pb.SmartServo.ServoType.AX12)
+            self.Servo_IO.moveSpeed(Actionneur.BrasPince.value, ValeurActionneur.BrasPinceIN.value,100, actionneurs_pb.SmartServo.ServoType.AX12)
             
     
     def stockConserve(self, stock: bool, sync:bool = False):
         if stock:
-            self.Servo_IO.moveSpeed(Actionneur.Rentreur, ValeurActionneur.RentreurIN, ValeurActionneur.STSLowSpeed)
+            self.Servo_IO.moveSpeed(Actionneur.Rentreur.value, ValeurActionneur.RentreurIN.value, ValeurActionneur.STSLowSpeed.value)
             
         else:
-            self.Servo_IO.moveSpeed(Actionneur.Rentreur, ValeurActionneur.RentreurOUT, ValeurActionneur.STSLowSpeed)
+            self.Servo_IO.moveSpeed(Actionneur.Rentreur.value, ValeurActionneur.RentreurOUT.value, ValeurActionneur.STSLowSpeed.value)
             
         
     def liftConserve(self,up:bool, sync:bool = False):
         if up:
-            self.Servo_IO.moveSpeed(Actionneur.AscenseurAimant, ValeurActionneur.AscenseurAimantUP, ValeurActionneur.STSLowSpeed)
+            self.Servo_IO.moveSpeed(Actionneur.AscenseurAimant.value, ValeurActionneur.AscenseurAimantUP.value, ValeurActionneur.STSLowSpeed.value)
             
         else:
-            self.Servo_IO.moveSpeed(Actionneur.AscenseurAimant, ValeurActionneur.AscenseurAimantDOWN, ValeurActionneur.STSLowSpeed)
+            self.Servo_IO.moveSpeed(Actionneur.AscenseurAimant.value, ValeurActionneur.AscenseurAimantDOWN.value, ValeurActionneur.STSLowSpeed.value)
             
             
     def grabLowConserve(self, grab : bool, sync:bool = False):
         if grab : 
-            self.Servo_IO.move(Actionneur.AimantBasGauche,ValeurActionneur.AimantBasGaucheGRAB)
-            self.Servo_IO.move(Actionneur.AimantBasDroit,ValeurActionneur.AimantBasDroitGRAB)
+            self.Servo_IO.move(Actionneur.AimantBasGauche.value,ValeurActionneur.AimantBasGaucheGRAB.value)
+            self.Servo_IO.move(Actionneur.AimantBasDroit.value,ValeurActionneur.AimantBasDroitGRAB.value)
             
         else: 
-            self.Servo_IO.move(Actionneur.AimantBasGauche,ValeurActionneur.AimantBasGaucheDROP)
-            self.Servo_IO.move(Actionneur.AimantBasDroit,ValeurActionneur.AimantBasDroitDROP)
+            self.Servo_IO.move(Actionneur.AimantBasGauche.value,ValeurActionneur.AimantBasGaucheDROP.value)
+            self.Servo_IO.move(Actionneur.AimantBasDroit.value,ValeurActionneur.AimantBasDroitDROP.value)
             
         
     def grabHighConserve(self, grab : bool, sync:bool = False):
         if grab : 
-            self.Servo_IO.move(Actionneur.AimantHautGauche,ValeurActionneur.AimantHautGaucheGRAB)
-            self.Servo_IO.move(Actionneur.AimantHautDroit,ValeurActionneur.AimantHautDroitGRAB)
+            self.Servo_IO.move(Actionneur.AimantHautGauche.value,ValeurActionneur.AimantHautGaucheGRAB.value)
+            self.Servo_IO.move(Actionneur.AimantHautDroit.value,ValeurActionneur.AimantHautDroitGRAB.value)
             
         else: 
-            self.Servo_IO.move(Actionneur.AimantHautGauche,ValeurActionneur.AimantHautGaucheDROP)
-            self.Servo_IO.move(Actionneur.AimantHautDroit,ValeurActionneur.AimantHautDroitDROP)
+            self.Servo_IO.move(Actionneur.AimantHautGauche.value,ValeurActionneur.AimantHautGaucheDROP.value)
+            self.Servo_IO.move(Actionneur.AimantHautDroit.value,ValeurActionneur.AimantHautDroitDROP.value)
             
