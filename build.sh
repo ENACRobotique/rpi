cf72cdda2f209976fc234a7f84816e4ff7dcac79
ENACRobotique <robotique.enac@gmail.com>
2025-05-20 01:50:23 +0200

    build smart servos drivers, and add missing python requirements

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ld06_driver_path=$SCRIPT_DIR/drivers/lidar/ld06_cpp
 vl53_driver_path=$SCRIPT_DIR/drivers/vl53cpp
+servos_driver_path=$SCRIPT_DIR/drivers/smart_servo
 
 rm -r $ld06_driver_path/build
 rm -r $vl53_driver_path/build
+rm -r $servos_driver_path/build
 
 # exit on error
 set -e
@@ -17,3 +19,7 @@ cmake --build $ld06_driver_path/build
 mkdir -p $vl53_driver_path/build
 cmake -S $vl53_driver_path -B $vl53_driver_path/build
 cmake --build $vl53_driver_path/build
+
+mkdir -p $servos_driver_path/build
+cmake -S $servos_driver_path -B $servos_driver_path/build
+cmake --build $servos_driver_path/build
