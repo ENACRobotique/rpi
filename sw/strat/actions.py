757a386f4eca276221f1ea2ba5a43255f96ef580
ENACRobotique <robotique.enac@gmail.com>
2025-05-28 02:11:08 +0200

    launch ecal recording on match start

--- sw/strat/actions.py
+++ sw/strat/actions.py
@@ -14,6 +14,7 @@ from typing import Callable
 from dataclasses import dataclass
 import time
 from locomotion import Velocity
+import subprocess
 
 ##########################
 ###  Action Banderole  ###
@@ -93,6 +94,10 @@ class MatchStartAction(Action):
             return 1e6
         else:
             return 0
+    
+    @staticmethod
+    def end_cb(robot: Robot, world: World, status: py_trees.common.Status) -> None:
+        subprocess.Popen(['ecal_rec', '-r', '110', '--activate'])
 
 ##########################
 ###   Action Go Home   ###
