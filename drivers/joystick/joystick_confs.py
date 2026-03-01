ed83b1a29ba25adbb46f79053ba69655e4018b88
ENACRobotique <enac.robotique@github.com>
2026-03-01 18:57:49 +0100

    Fix joystick

---
+++ drivers/joystick/joystick_confs.py
@@ -0,0 +1,40 @@
+
+ATTACK3_CONF = {
+    "X": 1, #axe
+    "X_sens" : -1, #facteur
+    "X_offset": 0.,
+    "X_dead_zone": 0.1,
+    "Y": 0, #axe
+    "Y_sens" : -1,
+    "Y_offset" : 0.,
+    "Y_dead_zone": 0.1,
+    "angle_gauche": 3, #bouton
+    "angle_droit":4, #bouton
+    "vitesse_supra_luminique": 0 #bouton
+}
+
+BATTLETRON = {
+    "X": 4, #axe 1
+    "X_sens" : -1, #facteur
+    "X_offset": 0.15,
+    "X_dead_zone": 0.1,
+    "Y": 3, #axe 0
+    "Y_sens" : -1,
+    "Y_offset" : -0.6,
+    "Y_dead_zone": 0.1,
+    "THETA": 0, #axe 3
+    "THETA_sens" : -1,
+    "THETA_offset" : 0.1, #à régler
+    "THETA_dead_zone": 0.1, # à régler
+    "theta_supra_luminique":11, #bouton
+    "vitesse_supra_luminique": 12, #bouton
+    "glisse" : 1, #hat (fleche haut et bas)
+    "selectGlisse" : 0, #hat (fleche gauche et droite) 
+    "bras" : 6, #bouton gachette gauche
+    "verrou" : 7, #bouton gachette droite
+    "grabHaut" : 4, #bouton bumper gauche
+    "grabBas" : 5, #bouton bumper droit
+    "action_1" : 0, #bouton bumper gauche
+    "action_2" : 2, #bouton bumper gauche
+
+}
