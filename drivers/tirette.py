e408d9112d45364bcf922b7736df07780ebc63e5
ENACRobotique <robotique.enac@gmail.com>
2026-05-15 08:00:21 +0200

    [tirette] ajot btn couleur

--- drivers/tirette.py
+++ drivers/tirette.py
@@ -10,6 +10,8 @@ import robot_state_pb2  as rpb
 LINE = 25
 TIMEOUT = 3
 
+LINE_COLOR = 24
+
 
 if __name__ == "__main__":
     tirette_request = gpiod.request_lines(
@@ -21,6 +23,12 @@ if __name__ == "__main__":
             bias=Bias.PULL_UP,
             edge_detection=Edge.BOTH,
             debounce_period=datetime.timedelta(seconds=0.1)
+        ),
+        LINE_COLOR: gpiod.LineSettings(
+            direction=Direction.INPUT,
+            bias=Bias.PULL_UP,
+            edge_detection=Edge.BOTH,
+            debounce_period=datetime.timedelta(seconds=0.1)
         )
         },
     )
@@ -28,6 +36,7 @@ if __name__ == "__main__":
     if not ecal_core.is_initialized():
             ecal_core.initialize("Tirette")
     tirette_pub = ProtoPublisher(rpb.Tirette, "tirette")
+    color_pub = ProtoPublisher(rpb.Side,"color")
     
     while ecal_core.ok():
         if tirette_request.wait_edge_events(timeout=TIMEOUT):
@@ -36,7 +45,13 @@ if __name__ == "__main__":
                 #print(event)
         val = tirette_request.get_value(LINE)
         tirette_state = rpb.Tirette.IN if val == Value.INACTIVE else rpb.Tirette.OUT
+        valColor = tirette_request.get_value(LINE_COLOR)
+        color_state = rpb.Side.BLUE if val == Value.INACTIVE else rpb.Side.YELLOW
+
         msg = rpb.Tirette(tirette_state=tirette_state)
         tirette_pub.send(msg)
         print(val, tirette_state)
+
+        msg_c = rpb.Side(color = color_state)
+        color_pub.send(msg_c)
 
